494dd7c2304e2430a447c1b61d72d23e164d1364
Andrew-Haaksma-students <207887@student.oaisd.org>
2024-10-08 13:46:31 +0000

    test 3

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.m/snew-console-template for more information
 using System;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("My name is Andrew Haaksma.")
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Andrew Haaksma.");
 
         //using Variables
         int age = 17;
         string greeting = "Hello World";
-        string first name = "Andrew";
+        string firstname = "Andrew";
         string lastname = "Haaksma";
         double money = 2.79;
 
-        Console.WriteLine(greeting + "!" + "My name is" + firstname + "" + lastname + ". I am" + age + " years old. I just spent $" + money + " on an Energy Drink")
+        Console.WriteLine(greeting + "!" + "My name is" + firstname + "" + lastname + ". I am" + age + " years old. I just spent $" + money + " on an Energy Drink");
     }
 }
