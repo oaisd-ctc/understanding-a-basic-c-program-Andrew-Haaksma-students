ecc336e4e06d8dde499f080f0f0b2789eeaeb08b
Andrew-Haaksma-students <207887@student.oaisd.org>
2024-10-08 13:45:24 +0000

    test 2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System;
 
 class Program
 {
-    public static void Main(string[ args])
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello World!")
         Console.WriteLine("My name is Andrew Haaksma.")
