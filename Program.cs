943e6b06c3b566c33f9651ad9390469a43a041a4
Andrew-Haaksma-students <207887@student.oaisd.org>
2024-10-08 13:50:14 +0000

    Understanding a Basic C# Program

--- Program.cs
+++ Program.cs
@@ -15,6 +15,6 @@ class Program
         string lastname = "Haaksma";
         double money = 2.79;
 
-        Console.WriteLine(greeting + "!" + "My name is" + firstname + "" + lastname + ". I am" + age + " years old. I just spent $" + money + " on an Energy Drink");
+        Console.WriteLine(greeting + "!" + "My name is" + firstname + "" + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink");
     }
 }
